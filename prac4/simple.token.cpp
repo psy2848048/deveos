27c6f45b99bac1fa88b4455404b093285de7097b
Bryan <psy2848048@gmail.com>
2019-01-24 15:53:25 +0900

    Add new prac4

---
+++ prac4/simple.token.cpp
@@ -0,0 +1,70 @@
+#include "simple.token.hpp"
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
+#include <string>
+
+using namespace eosio;
+namespace simpletoken {
+void token::add_balance(name payer, name to, asset q, std::string memo){
+    _accounts acc(_self, _self.value);
+
+    auto toitr = acc.find( to.value );
+    if (toitr == acc.end() ){
+        acc.emplace(payer, [&]( auto& a ){
+            a.owner = to;
+            a.balance = q;
+        });
+    } else {
+        acc.modify(toitr, _self, [&]( auto& a ){
+            a.balance += q;
+            eosio_assert( a.balance >= q, "overflow detected" );
+        });
+    }
+}
+
+void token::sub_balance(name payer, name to, asset q, std::string memo){
+    _accounts acc(_self, _self.value);
+
+    auto toitr = acc.find( to.value );
+    acc.modify( toitr, _self, [&]( auto& a ){
+        eosio_assert(a.balance >= q, "Not enough token balance" );
+        a.balance -= q;
+    });
+}
+
+void token::issue( name to, asset q, std::string memo){
+    add_balance( _self, to, q, memo);
+    _record rec(_self, _self.value);
+
+    rec.emplace(_self, [&](auto& a){
+        a.id = rec.available_primary_key();
+        a.from = _self;
+        a.to = to;
+        a.amount = q;
+        a.memo = memo;
+    });
+}
+
+void token::transfer( name from, name to, asset q, std::string memo){
+    add_balance( from, to, q, memo);
+    sub_balance( to, from, q, memo);
+    _record rec(_self, _self.value);
+
+    rec.emplace(from, [&](auto& a){
+        a.id = rec.available_primary_key();
+        a.from = from;
+        a.to = to;
+        a.amount = q;
+        a.memo = memo;
+    });
+}
+
+void token::delrecord(uint64_t rec_id){
+    _bidder bidderstable(_self, _self.value);
+
+    auto itr = bidderstable.find( rec_id );
+    bidderstable.erase( itr );
+}
+
+}
+EOSIO_DISPATCH( simpletoken::token, (issue)(transfer) )
